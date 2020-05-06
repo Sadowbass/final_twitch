a9f3a4059ebf36779eda9ffc5b4b40bcf254a618
GwanUkKim <guk1502012@gamil.com>
2020-05-06 17:27:02 +0900

    servlet_200506_17
    처음 ajax로 화면이동 성공(서블릿)

---
+++ src/main/java/streaming/ConUk.java
@@ -0,0 +1,21 @@
+package streaming;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ConUk {
+	@RequestMapping(value="/stream.st", method= {RequestMethod.GET, RequestMethod.POST})
+	public String stream(HttpServletRequest req) {
+		ModelAndView mv=new ModelAndView();
+
+		/*mv.setViewName("stream");*/
+		return "stream";
+
+	}
+
+}
