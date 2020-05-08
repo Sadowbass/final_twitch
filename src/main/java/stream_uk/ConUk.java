cf9b9de353be2fa8f09c3325a9c5b4c96dd76893
GwanUkKim <guk1502012@gamil.com>
2020-05-08 17:35:21 +0900

    fridayLast20050817
    금요일 마지막 푸쉬

--- src/main/java/stream_uk/ConUk.java
+++ src/main/java/stream_uk/ConUk.java
@@ -3,6 +3,7 @@ package stream_uk;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
