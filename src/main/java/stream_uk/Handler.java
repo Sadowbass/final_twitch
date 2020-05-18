3d6d8d29b65df934b69b4aaa8bf8229f673a4980
GwanUkKim <guk1502012@gamil.com>
2020-05-19 02:28:10 +0900

    현재 시청자, 누적 시청자 구현 완료
    내일 해야할것
    1. 영탁이형 페이지 자바스크립트 구현하기
    2. 동영상스트리밍 페이지 자바스크립트 구현하기

--- src/main/java/stream_uk/Handler.java
+++ src/main/java/stream_uk/Handler.java
@@ -23,15 +23,23 @@ public class Handler extends TextWebSocketHandler{
 	 * 입장한 사람(채팅 보냄)=>oid
 	 * 채팅 내용=>txt
 	 */
-	Map<WebSocketSession, String> everybody=new HashMap<WebSocketSession, String>(); /*모든 세션 <session,mid>*/
+	Map<WebSocketSession, String> everybody=new HashMap<WebSocketSession, String>(); /*모든 유저 <session,mid>*/
 	Map<String, Object[]> users=new HashMap<String, Object[]>(); /*로그인한 유저 목록 <oid,[mid,session]>*/
-	int accumulate=0; /*로그인한 유저 누적 숫자*/
+	Map<String, Integer> accumulate=new HashMap<String, Integer>(); /*누적 시창저 숫자*/
 	Gson gson=new Gson();
 	UserList userList=new UserList();
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		String mid=session.getUri().toString().substring(session.getUri().toString().lastIndexOf("?")+1);
+
+		/*누적 시청자 숫자*/
+
+		if(accumulate.get(mid)==null) {
+			accumulate.put(mid, 1);
+		}else {
+			accumulate.put(mid, accumulate.get(mid)+1);
+		}
 
 		everybody.put(session, mid); /*모든 세션 추가*/
 
@@ -43,11 +51,27 @@ public class Handler extends TextWebSocketHandler{
 		String oid="손님"+(session.getId()).substring(0, 5);
 		//String oid=(String)session.getAttributes().get("session_id"); /*로그인한 유저*/
 
-		System.out.println(mid+"의 방에 "+oid+"님이 입장하였습니다.");
-		JsonObject addUser=new JsonObject(); /*기존 유저에게 보낼 새로 들어온 유저 아이디 담을 제이슨 객체*/
+		/*새로 들어온 유저에게 mid가 같은 사람 목록 보내줌 (출력해야할 로그인유저 리스트)*/
+		List<String> usersList=new ArrayList<String>();
+		Iterator<String> usersIterator=users.keySet().iterator();
+		while(usersIterator.hasNext()) {
+			String user=usersIterator.next();
+			if(users.get(user)[0].equals(mid)) {
+				usersList.add(user);
+			}
+		}
+		Map<String, Object> usersAndAcc=new HashMap<String, Object>(); /*유저목록 & 누적유저수*/
+		usersAndAcc.put("users",usersList);
+		String jsonUsersAndAcc=gson.toJson(usersAndAcc);
+		session.sendMessage(new TextMessage(jsonUsersAndAcc)); /*새로 들어온 사람에게 유저목록 & 누적유저수 보냄*/
+
+		JsonObject addAndAcc=null;
 		if(oid!=null) {
 			users.put(oid, body); /*유저 목록에 로그인한 유저 추가*/
-			addUser.addProperty("addUser", oid); /*기존 유저에게 보낼 새로 들어온 유저 아이디 추가*/
+
+			addAndAcc=new JsonObject(); /*모든 유저에게 보낼 새로 들어온 유저 아이디 담을 제이슨 객체*/
+			addAndAcc.addProperty("addUser", oid); /*모든 유저에게 보낼 새로 들어온 유저 아이디 추가*/
+			addAndAcc.addProperty("accUser", accumulate.get(mid));
 
 			/*userList에 저장*/
 			userList.setMid(mid);
@@ -57,23 +81,8 @@ public class Handler extends TextWebSocketHandler{
 			dao.enter(userList);
 		}
 
-
-		/*새로 들어온 유저에게 mid가 같은 사람 목록 보내줌 (출력해야할 로그인유저 리스트)*/
-		List<String> userList=new ArrayList<String>();
-		Iterator<String> usersIterator=users.keySet().iterator();
-		while(usersIterator.hasNext()) {
-			String user=usersIterator.next();
-			if(users.get(user)[0].equals(mid)) {
-				userList.add(user);
-			}
-		}
-		Map<String, Object> usersAndAcc=new HashMap<String, Object>(); /*유저목록 & 누적유저수*/
-		usersAndAcc.put("users",userList);
-		String jsonUsersAndAcc=gson.toJson(usersAndAcc);
-		session.sendMessage(new TextMessage(jsonUsersAndAcc)); /*새로 들어온 사람에게 유저목록 & 누적유저수 보냄*/
-
-		/*기존 유저들에게 새로 들어온 유저 아이디 보내줌*/
-		String jsonAddUser=gson.toJson(addUser);
+		/*모든 유저들에게 새로 들어온 유저 아이디 보내줌*/
+		String jsonAddUser=gson.toJson(addAndAcc);
 		Iterator<WebSocketSession> sessionIterator=everybody.keySet().iterator();
 		while(sessionIterator.hasNext()) {
 			WebSocketSession target=sessionIterator.next();
@@ -103,8 +112,6 @@ public class Handler extends TextWebSocketHandler{
 
 			}
 		}
-
-		System.out.println(mid+"방에서 "+cht.getCht_oid()+"님이 :::::::::"+message.getPayload());
 	}
 
 	@Override
@@ -116,11 +123,11 @@ public class Handler extends TextWebSocketHandler{
 		String oid="손님"+(session.getId()).substring(0, 5);
 		//String oid=(String)session.getAttributes().get("session_id"); /*위에꺼 지우고 이거 넣어야함(유저삭제부분)*/
 
-		JsonObject delUser=new JsonObject(); /*기존 유저에게 보낼 사제할 유저 아이디 담을 제이슨 객체*/
+		JsonObject delUser=new JsonObject(); /*모든 유저에게 보낼 사제할 유저 아이디 담을 제이슨 객체*/
 
 		if(oid!=null) {
 			users.remove(oid); /*로그인한 유저 삭제*/
-			delUser.addProperty("delUser", oid); /*기존 유저에게 보낼 삭제할 유저 아이디 추가*/
+			delUser.addProperty("delUser", oid); /*모든 유저에게 보낼 삭제할 유저 아이디 추가*/
 
 
 			userList.setMid(mid);
@@ -130,24 +137,6 @@ public class Handler extends TextWebSocketHandler{
 			dao.exit(userList);
 		}
 
-		System.out.println(mid+"의 방에서 "+oid+"님이 퇴장하였습니다.");
-
-		/*List<String> userList=new ArrayList<String>();
-
-		users mid중에 들어온 사람의 mid가 같아야 출력해야함 (출력해야할 로그인유저 리스트)
-		Iterator<String> usersIterator=users.keySet().iterator();
-		while(usersIterator.hasNext()) {
-			String user=usersIterator.next();
-			if(users.get(user)[0].equals(mid)) {
-				userList.add(user);
-
-			}
-		}*/
-
-		/*Map<String, Object> usersAndAcc=new HashMap<String, Object>(); 유저목록 & 누적유저수
-		usersAndAcc.put("users",userList);
-
-		String jsonUsersAndAcc=gson.toJson(usersAndAcc);*/
 
 		String jsonDelUser=gson.toJson(delUser);
 		Iterator<WebSocketSession> sessionIterator=everybody.keySet().iterator();
